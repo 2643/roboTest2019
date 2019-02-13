33e07443d9f331a68abffc650355ca18b45ddc41
pimpale <gpimpale29@gmail.com>
2019-02-12 20:44:45 -0800

    added new modes

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -19,6 +19,7 @@ public class Robot extends TimedRobot {
   WPI_TalonSRX r1 = new WPI_TalonSRX(16);
   WPI_TalonSRX r2 = new WPI_TalonSRX(1);
 
+  private static volatile byte state = 0;
 
   void set(double l, double r) {
     l1.set(l);
@@ -43,30 +44,46 @@ public class Robot extends TimedRobot {
 
   @Override
   public void autonomousPeriodic() {
-    double centerLocation = (
-      visionTable.getEntry("centroid-left-x").getDouble(0) +
-      visionTable.getEntry("centroid-right-x").getDouble(0)) / 2;
-    boolean valid = visionTable.getEntry("valid").getBoolean(false);
-    System.out.println("valid: " + valid + "center: " + centerLocation);
-    if(valid) {
-      if(centerLocation > 0.1) 
-      {
-        set(0.3, -0.3);
-      } else if(centerLocation < -0.1)
-      {
-        set(-0.3, 0.3);
-      } else {
-        set(0.3, 0.3);
-      }
-    } else {
-      set(0,0);
-    }
-    
+
   }
 
   @Override
   public void teleopPeriodic() {
-    set(joystick.getRawAxis(1), joystick.getRawAxis(5));
+    if(joystick.getRawButton(1)) {
+      state = 2;
+    } else if(joystick.getRawButton(1)) {
+      state = 1;
+    } else {
+      state = 0;
+    }
+
+    if (state == 0) {
+      set(
+        -Math.pow(joystick.getRawAxis(1), 5), 
+        -Math.pow(joystick.getRawAxis(5), 5)
+        );
+    } else if(state == 1) {
+      set(
+        -Math.pow(joystick.getRawAxis(1), 1), 
+        -Math.pow(joystick.getRawAxis(5), 1)
+        );
+    } else if (state == 2) {
+      double centerLocation = (visionTable.getEntry("centroid-left-x").getDouble(0)
+          + visionTable.getEntry("centroid-right-x").getDouble(0)) / 2;
+      boolean valid = visionTable.getEntry("valid").getBoolean(false);
+      System.out.println("valid: " + valid + "center: " + centerLocation);
+      if (valid) {
+        if (centerLocation > 0.1) {
+          set(0.3, -0.3);
+        } else if (centerLocation < -0.1) {
+          set(-0.3, 0.3);
+        } else {
+          set(0.3, 0.3);
+        }
+      } else {
+        set(0, 0);
+      }
+    }
   }
 
   @Override
